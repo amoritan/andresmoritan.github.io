8adb5920d9b2941d840687f3ccb8784f056f32b6
Andres Moritan <amoritan@me.com>
2017-10-07 00:23:06 -0300

    clean files

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,22 @@
 <template>
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
-    </main>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
-<style>
-body {
-  margin: 0;
-}
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Mada:300,400,700');
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
+  body {
+    margin: 0;
+    font-family: 'Mada', Helvetica, Arial, sans-serif;
+    color: $white;
+    background: $black;
+  }
 </style>
