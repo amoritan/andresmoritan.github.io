45582474714488b8bae278d6161478a0de68f16d
Andres Moritan <amoritan@me.com>
2020-01-26 11:53:58 +0000

    Initial project customization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Hi, I'm <abbr title="Andres Moritan">Andi</abbr></h1>
+        <h2>I bring people together to create awesome digital products</h2>
       </header>
     </div>
   );
